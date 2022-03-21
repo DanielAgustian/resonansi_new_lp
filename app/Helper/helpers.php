2d8985e11711047274ea3b7fd60546c1c7055cee
Daniel <danielagustianyoali@gmail.com>
2022-03-21 16:16:45 +0700

    commit daya

--- app/Helper/helpers.php
+++ app/Helper/helpers.php
@@ -14,3 +14,19 @@ function set_none_navbar($uri, $output = ' d-none')
    }
  }
 }
+
+function set_none_list( $output = ' d-none')
+{
+ $uri = ['marketplacePageV2', 'socMedPageV2', 'landingPageV2', 'warehousePageV2', 'conversionPageV2'];
+ if( is_array($uri) ) {
+   foreach ($uri as $u) {
+     if (Route::is($u)) {
+       return $output;
+     }
+   }
+ } else {
+   if (Route::is($uri)){
+     return $output;
+   }
+ }
+}
--- resources/views/components/new/header.blade.php
+++ resources/views/components/new/header.blade.php
@@ -66,7 +66,7 @@
 
                   </div>
 
-                  <div id="primary-menu-trigger" class="{{set_none_navbar('marketplacePageV2')}}">
+                  <div id="primary-menu-trigger" class="{{set_none_list()}}">
                       <svg class="svg-trigger" viewBox="0 0 100 100">
                           <path
                               d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20">
@@ -80,7 +80,7 @@
 
                   <!-- Primary Navigation
       ============================================= -->
-                  <nav class="primary-menu with-arrows not-dark {{set_none_navbar('marketplacePageV2')}}">
+                  <nav class="primary-menu with-arrows not-dark {{set_none_list()}}">
 
                       <ul class="menu-container ">
                           <li class="menu-item current"><a class="menu-link" href="{{ route('aboutPage') }}">
