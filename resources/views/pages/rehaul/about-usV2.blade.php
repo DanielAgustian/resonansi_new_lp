0ad5f235b2678c7a40bf77908288a5c166477abc
Daniel <danielagustianyoali@gmail.com>
2022-03-25 13:48:50 +0700

    commit revisi

--- resources/views/pages/rehaul/about-usV2.blade.php
+++ resources/views/pages/rehaul/about-usV2.blade.php
@@ -102,7 +102,7 @@
 
             <div class="clear"></div>
 
-            <div class="py-6 mt-5 dark" style="background-color: #061a35;">
+            <div class="py-6 mt-5 dark" style="background-color: #061a35; margin-bottom: 0px; padding-bottom: 60px">
                 <div class="container mb-5">
                     <div class="row justify-content-between">
                         <div class="col-md-5">
@@ -133,9 +133,9 @@
 
             <div class="clear"></div>
 
-            <div class="section dark pt-0 mb-0 bg-color"
+            <div class="section dark pt-0 mt-0 mb-0 bg-color"
                 style="background: url('demos/movers/images/bg-2.png') no-repeat center bottom / 100%; overflow: visible">
-                <svg viewBox="0 0 1960 206.8" class="bg-white">
+                <svg viewBox="0 0 1960 206.8" style="background-color: #061a35">
                     <path class="svg-themecolor" style="opacity:0.2;"
                         d="M0,142.8A2337.49,2337.49,0,0,1,297.5,56.3C569.33-3.53,783.89.22,849.5,2.3c215.78,6.86,382.12,45.39,503.25,73.45,158.87,36.8,283.09,79.13,458.75,54.55A816.49,816.49,0,0,0,1983,86.8v110H0Z" />
                     <path class="svg-themecolor" d="M.5,152.8s498-177,849-150,1031,238,1134,94v110H.5Z" />
@@ -143,9 +143,9 @@
                 <div class="container">
                     <div class="row align-items-center justify-content-center center my-4">
 
-                        <div class="col-sm-8">
+                        <div class="col-sm-10">
                             <div class="heading-block border-bottom-0 mb-4">
-                                <h2 class="fw-semibold ls0 nott mb-3" style="font-size: 44px; line-height: 1.3">Ingin Mengetahui Layanan Kami Lebih Lanjut?</h2>
+                                <h2 class="fw-semibold ls0 nott mb-3" style="font-size: 31px; line-height: 1.3">Ingin Mengetahui Layanan Kami Lebih Lanjut?</h2>
                                 <p>Klik tombol di bawah ini jika ingin mengetahui lebih lanjut tentang apa yang bisa dilakukan Resonansi!</p>
                             </div>
                             <a href="demo-movers-contact.html"
